550f75eedbcaad3f247c3a3ed9840aed88f72103
Asetr <asetre.paul@gmail.com>
2017-12-08 10:52:54 -0800

    model fixes date, description

--- model.js
+++ model.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 const {Schema} = mongoose
 
-//use golabl promise instead of mongoose promise
+//use global promise instead of mongoose promise
 mongoose.Promise = global.Promise
 
 var postsSchema = new Schema({
     title: {type: String, required: true},
     email: {type: String, required: true},
-    description: {type: Date, required: true}
-
+    description: {type: String},
+    date: {type: Date, required: true}
 })
 
 const Posts = mongoose.model('Posts', postsSchema)
