543525ef8fddcfa2343a89081f73b0822ac86ea3
Asetr <asetre.paul@gmail.com>
2017-12-08 10:37:32 -0800

    Added model

---
+++ model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+{Schema} = mongoose
+
+//use golabl promise instead of mongoose promise
+mongoose.Promise = global.Promise
+
+var postsSchema = new Schema({
+    title: {type: String, required: true},
+    email: {type: String, required: true},
+    description: {type: Date, required: true}
+
+})
+
+const Posts = mongoose.model('Posts', postsSchema)
+
+module.exports = Posts
